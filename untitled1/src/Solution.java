4a6b5982fbd90bc30a8c0887943cd132bafc0389
John Doe <you@example.com>
2021-01-17 10:40:24 +0300

    update

--- untitled1/src/Solution.java
+++ untitled1/src/Solution.java
@@ -1,8 +1,7 @@
 package com.javarush.task.task13.task1326;
 
-/* 
+/*
 	Прочитать файл построчно и вывести в консоль
-	c:\Materials\Java_materials\BufferedFileReader.java
 */
 
 import java.io.*;
@@ -14,21 +13,21 @@ public class Solution {
         String fileName = reader.readLine();
         reader.close();
         reader = null;
-		
-		try { 
-			File file = new File(fileName);
-			FileReader fr = new FileReader(file);
-			BufferedReader r = new BufferedReader(fr);
-			String line;
-			while ((line = r.readLine()) != null ){
-				System.out.println(line);
-			}
-			r.close();
-			fr.close();
-			file = null;
-		// в идеале, конечно, надо более тщательно распределить блоки try..catch
-		// чтобы закрыть файл более корректно
-		} catch (FileNotFoundException e) {
+
+        try {
+            File file = new File(fileName);
+            FileReader fr = new FileReader(file);
+            BufferedReader r = new BufferedReader(fr);
+            String line;
+            while ((line = r.readLine()) != null ){
+                System.out.println(line);
+            }
+            r.close();
+            fr.close();
+            file = null;
+            // в идеале, конечно, надо более тщательно распределить блоки try..catch
+            // чтобы закрыть файл более корректно
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
